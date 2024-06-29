4adda9de3f3d5aea73cee9cb178312a2a39392d7
MIGHits <164134306+MIGHits@users.noreply.github.com>
2024-06-29 07:06:37 +0700

    fix

--- Assets/Scripts/Boss/DefaultAttack.cs
+++ Assets/Scripts/Boss/DefaultAttack.cs
@@ -76,21 +76,6 @@ namespace Core.AI
         {
             Gizmos.DrawWireSphere(attackArea.position, attackRange);
         }
-        public void Reflect()
-        {
-            if ((player.transform.position.x < transform.position.x && IsFacingRight && !IsAttacking)
-                || (player.transform.position.x > transform.position.x && !IsFacingRight && !IsAttacking))
-            {
-                IsFacingRight = !IsFacingRight;
-                animator.SetBool("IsRight", IsFacingRight);
-
-                attackArea.transform.localPosition = new Vector3(-attackArea.transform.localPosition.x,
-                    attackArea.transform.localPosition.y, attackArea.transform.localPosition.z);
-
-                backwardAttackArea.transform.localPosition = new Vector3(-backwardAttackArea.transform.localPosition.x,
-                    backwardAttackArea.transform.localPosition.y, backwardAttackArea.transform.localPosition.z);
-            }
-        }
     }
 
 }
